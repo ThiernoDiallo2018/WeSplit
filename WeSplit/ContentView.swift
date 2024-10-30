242cb5d220f3576d8ac9cce9a22cfcb1ee694822
Thierno Diallo <tdiallo1996@gmail.com>
2024-10-30 00:11:07 -0400

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Thierno Diallo on 10/29/24.
+//
+// Views are just a function of their state
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    ContentView()
+}
