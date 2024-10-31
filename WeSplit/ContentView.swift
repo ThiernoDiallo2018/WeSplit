0d348255a783fd80c4a1dd5395147889ca381c03
Thierno Diallo <tdiallo1996@gmail.com>
2024-10-30 21:47:32 -0400

    Completed WeSplit App

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,9 +9,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [15, 20, 25, 30, 35, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople)
+        let tipSelection = Double(tipPercentage) / 100
+        let tipValue = checkAmount * tipSelection
+        
+        let total = tipValue + checkAmount
+        let perPerson = total / peopleCount
+        
+        return perPerson
+    }
+ 
     
     var body: some View {
-        Text("Hello World")
+        NavigationStack {
+            Form {
+                Section("Check Amount") {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Numeber of People", selection: $numberOfPeople) {
+                        ForEach(0..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.menu)
+                }
+                Section("Percentage") {
+                    Text("How much do you want to top?")
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                
+                    
+                }
+                Section("Final Amount") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD" ))
+                }
+            }.navigationTitle("WeSplit").navigationBarTitleDisplayMode(.inline)
+        
+
+        }
     }
 }
 
