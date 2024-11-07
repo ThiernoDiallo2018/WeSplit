4a5831de14ee671ac7c4c756730b80af625ff3ed
Thierno Diallo <tdiallo1996@gmail.com>
2024-11-06 22:31:11 -0500

    Completed WeSplit App

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,8 +12,19 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [15, 20, 25, 30, 35, 0]
+    
+    var totalBeforeSplit: Double {
+        
+        let tipSelection = Double(tipPercentage) / 100
+        let tipValue = checkAmount * tipSelection
+        
+        let total = checkAmount + tipValue
+        
+        return total
+    }
     
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople)
@@ -33,29 +44,43 @@ struct ContentView: View {
                 Section("Check Amount") {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
                     Picker("Numeber of People", selection: $numberOfPeople) {
                         ForEach(0..<100) {
                             Text("\($0) people")
                         }
                     }
-                    .pickerStyle(.menu)
+                    .pickerStyle(.navigationLink)
                 }
                 Section("Percentage") {
-                    Text("How much do you want to top?")
+                    Text("How much do you want to tip?")
                     Picker("Tip Percentage", selection: $tipPercentage) {
                         ForEach(tipPercentages, id: \.self) {
-                            Text("\($0)")
+                            Text($0, format: .percent)
                         }
                     }
                     .pickerStyle(.segmented)
                 
                     
                 }
-                Section("Final Amount") {
+                
+                Section("Check Value + Tip") {
+                    Text(totalBeforeSplit, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section("Amount Per Person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD" ))
                 }
-            }.navigationTitle("WeSplit").navigationBarTitleDisplayMode(.inline)
+            }.navigationTitle("WeSplit")
+             .navigationBarTitleDisplayMode(.inline)
+             .toolbar {
+                    if amountIsFocused {
+                        Button("Done") {
+                            amountIsFocused = false
+                        }
+                    }
+                }
         
 
         }
